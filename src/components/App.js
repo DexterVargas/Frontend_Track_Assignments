c4534c34261a5b05022f3986f1c1a307c635391b
dextervargasDev <dextervargas0912@gmail.com>
2023-04-01 13:27:23 +0800

    assignment contactlist. done. modified a lot

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import ContactForm from './ContactForm';
+import NavbarSection from './NavbarSection';
+import Footer from './Footer';
+function App() {
+    return (
+        <>
+            <NavbarSection />
+            <ContactForm />
+            <Footer />
+
+        </>
+    );    
+}
+export default App;
